b75188a0db96406759f50df9a47d2fc39eadfeb2
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-03-23 14:55:09 -0700

    Update index.js

--- buffer/index.js
+++ buffer/index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const testStringBuffer = str => {
+     const buf = Buffer.from(str, 'utf8')
+     console.log(buf)
+     console.log(buf[0])
+     console.log(buf.toString())
+}
+
+try {
+     testStringBuffer("hello world!")
+     testStringBuffer("greetings celestial sphere")
+} catch (e) {
+     console.error(e)
+}
