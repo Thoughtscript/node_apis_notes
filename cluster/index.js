f7d86901971a4aa02a9efbea8b67920164f6b540
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-03-23 15:35:24 -0700

    Update index.js

--- cluster/index.js
+++ cluster/index.js
@@ -1,1 +1,29 @@
 'use strict'
+
+/**
+ * @author - Adam In Tae Gerard - https://www.linkedin.com/in/adamintaegerard/
+ *
+ * Core cluster logic.
+ */
+
+const CL = require('cluster/cluster'),
+    C = require('../../config')
+
+module.exports = {
+    createHttpCluster: () => {
+        if (CL.isMaster) {
+            let cpuCount = require('os').cpus().length
+            if (C.CLUSTER != null) cpuCount = C.CLUSTER
+            for (let i = 0; i < cpuCount; i++) {
+                CL.fork()
+            }
+
+            CL.on('fork', worker => console.log(`Worker %d created: ${worker.id}`))
+                .on('exit', worker => {
+                    console.error(`Worker %d died: ${worker.id}`)
+                    CL.fork()
+                })
+
+        } else require('../server/http').createHttpServer(C.PORT)
+    }
+}
